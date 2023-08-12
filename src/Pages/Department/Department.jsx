8b76a460f03263b9c45629a4f53e797d0a2152e1
krunal002 <109591661+krunal002@users.noreply.github.com>
2023-08-12 21:58:16 +0530

    update

--- src/Pages/Department/Department.jsx
+++ src/Pages/Department/Department.jsx
@@ -1,10 +1,55 @@
-import "./Department.css";
+import "./Department.css"
+import React, { useContext } from "react";
+
+import { useNavigate } from "react-router";
+import { DataContext } from "../../App";
 
 const Department = () => {
-  return (
-    <div>
-      <h1>Department</h1>
-    </div>
-  );
+    const { setFilters } = useContext(DataContext);
+    const navigate = useNavigate();
+
+    return (
+        <div className="departments_wrapper">
+            <div className="dept_cards_container">
+                <div
+                    className="dept-cont"
+                    onClick={() => {
+                        navigate("/products");
+                        setFilters((prev) => ({
+                            ...prev,
+                            dept_type: "kitchen",
+                        }));
+                    }}
+                >
+                    Kitchen
+                </div>
+                <div
+                    className="dept-cont"
+                    onClick={() => {
+                        navigate("/products");
+                        setFilters((prev) => ({
+                            ...prev,
+                            dept_type: "clothing",
+                        }));
+                    }}
+                >
+                    Clothing
+                </div>
+                <div
+                    className="dept-cont"
+                    onClick={() => {
+                        navigate("/products");
+                        setFilters((prev) => ({
+                            ...prev,
+                            dept_type: "toys",
+                        }));
+                    }}
+                >
+                    Toys
+                </div>
+            </div>
+        </div>
+    );
 };
+
 export default Department;
